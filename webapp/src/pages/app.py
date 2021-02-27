64a17abef096ca70a259726893cc8617b3473c6a
Sarah-chu <13205641@life.hkbu.edu.hk>
2021-02-27 18:57:32 +0100

    add extra datasets
    modify the cleaning step of the app,
    please use the cleaned dataset('twitter_hate_speech_cleaned.csv')

--- webapp/src/pages/app.py
+++ webapp/src/pages/app.py
@@ -13,6 +13,17 @@ from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer 
 from sklearn.feature_extraction.text import TfidfVectorizer
 
+def clean_tweet(tweet): # for cleaning the sentence
+    my_punctuation = '!"$%&\'()*+,-./:;<=>?[\\]\\\\\^_`{|}~•@#'
+    tweet = re.sub('(RT\s@[A-Za-z0-9-_]+[A-Za-z0-9-_]+)', '', tweet) # remove retweet
+    tweet = re.sub('(@[A-Za-z0-9-_]+[A-Za-z0-9-_]+)', '', tweet)
+    tweet = tweet.lower() # lower case
+    tweet = re.sub('['+my_punctuation + ']+', ' ', tweet) # strip punctuation
+    tweet = re.sub('([0-9]+)', '', tweet) # remove numbers
+    tweet = re.sub('amp', '', tweet) # remove amp
+    tweet = re.sub('\s+', ' ', tweet) #remove double spacing
+    return tweet
+
 def write():
     tweet_input = st.beta_container()
     model_results = st.beta_container()
@@ -26,16 +37,18 @@ def write():
     with model_results:    
         if st.button("Predict"): 
             # removing punctuation
-            user_text = re.sub('[%s]' % re.escape(string.punctuation), '', user_text)
+            #user_text = re.sub('[%s]' % re.escape(string.punctuation), '', user_text)
+            user_text = clean_tweet(user_text)
             # tokenizing
             stop_words = set(stopwords.words('english'))
-            tokens = nltk.word_tokenize(user_text)
+            tokens = nltk.word_tokenize(stop_words)
             # removing stop words
             stopwords_removed = [token.lower() for token in tokens if token.lower() not in stop_words]
             # taking root word
             lemmatizer = WordNetLemmatizer() 
             lemmatized_output = []
-            for word in stopwords_removed:
+            #for word in stopwords_removed:
+            for word in tokens:
                 lemmatized_output.append(lemmatizer.lemmatize(word))
 
             # instantiating count vectorizor
